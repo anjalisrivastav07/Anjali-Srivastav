2d21f566851425918a8c773d65353b87aabb71c9
Anjali Srivastav <anjalisrivastav7066@gmail.com>
2026-02-06 22:19:24 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Hero from "./sections/Hero";
+import About from "./sections/About";
+import Skills from "./sections/Skills";
+import Experience from "./sections/Experience";
+import Contact from "./sections/Contact";
+
+export default function App() {
+  return (
+    <>
+      <Hero />
+      <About />
+      <Skills />
+      <Experience />
+      <Contact />
+    </>
+  );
+}
